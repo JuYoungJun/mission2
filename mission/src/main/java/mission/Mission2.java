b0bc3cda1ce59d9e9390019d0446eccf3d049d02
mandoo <kaks162@gmail.com>
2024-05-14 11:23:54 +0900

    100까지 출력 으로 해서 합으로 수정함

--- mission/src/main/java/mission/Mission2.java
+++ mission/src/main/java/mission/Mission2.java
@@ -3,9 +3,11 @@ package mission;
 public class Mission2 {
 
 	public static void main(String[] args) {
+		int sum = 0;
 		for (int i = 1; i <= 100; i++) {
-			System.out.println(i);
+			sum += i;
 		}
+		System.out.println("1부터 100까지의 합: " + sum);
 	}
 
 }
